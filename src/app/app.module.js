c4218fca078d13d720a51a9b86e1697d5c722b6a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-04-01 22:45:43 +0000

    day2, add components and routing

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -9,22 +9,43 @@ var platform_browser_1 = require('@angular/platform-browser');
 var core_1 = require('@angular/core');
 var forms_1 = require('@angular/forms');
 var http_1 = require('@angular/http');
+var router_1 = require('@angular/router');
 var material_1 = require('@angular/material');
 require('hammerjs');
 var app_component_1 = require('./app.component');
+var products_component_1 = require('./components/products/products.component');
+var product_detail_component_1 = require('./components/product-detail/product-detail.component');
+var cart_component_1 = require('./components/cart/cart.component');
+var order_component_1 = require('./components/order/order.component');
+var customers_component_1 = require('./components/customers/customers.component');
+var customer_detail_component_1 = require('./components/customer-detail/customer-detail.component');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             declarations: [
-                app_component_1.AppComponent
+                app_component_1.AppComponent,
+                products_component_1.ProductsComponent,
+                product_detail_component_1.ProductDetailComponent,
+                cart_component_1.CartComponent,
+                order_component_1.OrderComponent,
+                customers_component_1.CustomersComponent,
+                customer_detail_component_1.CustomerDetailComponent
             ],
             imports: [
                 platform_browser_1.BrowserModule,
                 forms_1.FormsModule,
                 http_1.HttpModule,
-                material_1.MaterialModule
+                material_1.MaterialModule,
+                router_1.RouterModule.forRoot([
+                    { path: 'products', component: products_component_1.ProductsComponent },
+                    { path: 'product', component: product_detail_component_1.ProductDetailComponent },
+                    { path: 'cart', component: cart_component_1.CartComponent },
+                    { path: 'order', component: order_component_1.OrderComponent },
+                    { path: 'customers', component: customers_component_1.CustomersComponent },
+                    { path: 'customer', component: customer_detail_component_1.CustomerDetailComponent }
+                ])
             ],
             providers: [],
             bootstrap: [app_component_1.AppComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { CartComponent } from './components/cart/cart.component';
+import { OrderComponent } from './components/order/order.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    CartComponent,
+    OrderComponent,
+    CustomersComponent,
+    CustomerDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductsComponent },
+      { path: 'product', component: ProductDetailComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'order', component: OrderComponent },
+      { path: 'customers', component: CustomersComponent },
+      { path: 'customer', component: CustomerDetailComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
